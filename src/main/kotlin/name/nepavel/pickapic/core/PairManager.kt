631631a9c87783b232371f79affa80fb0f86c252
Krizhanovskiy Pavel <pavel.krizhanovskiy@tieto.com>
2020-03-07 15:19:57 +0300

    Improved pairing

--- src/main/kotlin/name/nepavel/pickapic/core/PairManager.kt
+++ src/main/kotlin/name/nepavel/pickapic/core/PairManager.kt
@@ -1,57 +1,54 @@
 package name.nepavel.pickapic.core
 
 import name.nepavel.pickapic.domain.Pic
-import name.nepavel.pickapic.repository.DbHelper
 import name.nepavel.pickapic.repository.PicRepository
 import name.nepavel.pickapic.repository.ViewsRepository
+import kotlin.math.max
+import kotlin.math.min
+import kotlin.random.Random
 
 object PairManager {
 
     fun getNextPair(chatId: Long, votingName: String): Pair<Pic, Pic>? {
-        val closest = DbHelper.db.getMap<Long, Boolean>("CLOSEST").getOrDefault(chatId, false)
-        val list = getList(votingName, chatId, closest)
-        return if (list.isEmpty()) {
-            null
-        } else {
-            list.random()
-        }?.let {
-            DbHelper.db.getMap<Long, Boolean>("CLOSEST")[chatId] = !closest
-            ViewsRepository.increment(chatId, votingName, it.first.file_id)
-            ViewsRepository.increment(chatId, votingName, it.second.file_id)
-            it
-        }
+        return (getPairViewLimit(chatId, votingName, 1)
+            ?: getPairViewLimit(chatId, votingName, 2))
+            ?.let {
+                ViewsRepository.increment(chatId, votingName, it.first.file_id)
+                ViewsRepository.increment(chatId, votingName, it.second.file_id)
+                it
+            }
     }
 
-    private fun getList(voting: String, chatId: Long, closest: Boolean): List<Pair<Pic, Pic>> {
-        var list = getListWithViewLimit(chatId, voting, closest, 1)
-        if (list.size < 2) {
-            list = getListWithViewLimit(chatId, voting, closest, 2)
-        }
-        return list
-    }
-
-    private fun getListWithViewLimit(
+    private fun getPairViewLimit(
         chatId: Long,
         voting: String,
-        closest: Boolean,
         viewsLimit: Int
-    ): List<Pair<Pic, Pic>> {
-        return if (closest)
-            PicRepository.list(voting).filter { pic ->
-                ViewsRepository.get(
-                    chatId,
-                    voting,
-                    pic.file_id
-                ) < viewsLimit
-            }.sortedBy { it.rank }.windowed(2, 2) { it[0] to it[1] }
-        else {
-            val evens = PicRepository.list(voting).filter { pic -> ViewsRepository.get(chatId, voting, pic.file_id) < viewsLimit }
-                .filterIndexed { index, _ -> index % 2 == 1 }
-                .sortedBy { it.rank }
-            val odds = PicRepository.list(voting).filter { pic -> ViewsRepository.get(chatId, voting, pic.file_id) < viewsLimit }
-                .filterIndexed { index, _ -> index % 2 == 0}
-                .sortedBy { it.rank }
-            evens.zip(odds)
+    ): Pair<Pic, Pic>? {
+        val list = PicRepository.list(voting).filter { pic ->
+            ViewsRepository.get(
+                chatId,
+                voting,
+                pic.file_id
+            ) < viewsLimit
+        }.sortedBy { it.rank }
+        return when {
+            list.size < 2 -> null
+            list.size == 2 -> list[0] to list[1]
+            list.size in 3..6 -> list.shuffled().windowed(2, 2) { it[0] to it[1] }.random()
+            list.size > 6 -> {
+                val rndLeftIndex = Random.nextInt(list.size / 3 - 1, 2 * list.size / 3 + 1)
+                val left = list[rndLeftIndex]
+                val chooseRightFrom = list.subList(
+                    max(0, rndLeftIndex - list.size / 10),
+                    min(list.size - 1, rndLeftIndex + list.size / 10)
+                )
+                var right: Pic = chooseRightFrom.random()
+                while(left == right) {
+                    right = chooseRightFrom.random()
+                }
+                left to right
+            }
+            else -> throw IllegalStateException("List size is ${list.size}, no 'when' branch for it, WTF?!")
         }
     }
 }
